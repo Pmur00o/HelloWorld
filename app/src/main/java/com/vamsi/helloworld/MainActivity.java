c4cd89b0aeb78d5c15e4866d3d53a4f1c489ae62
Vamsi Mohan Krishna Vadrevu <vamsi.vadrevu@gmail.com>
2015-08-08 13:15:59 +0530

    Added a button and a new activity file.

--- app/src/main/java/com/vamsi/helloworld/MainActivity.java
+++ app/src/main/java/com/vamsi/helloworld/MainActivity.java
@@ -5,14 +5,17 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
+import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
 
+    Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
 }
